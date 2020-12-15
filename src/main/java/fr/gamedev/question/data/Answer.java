76ea88b9af55482f16ed172409479a2e962bfe7b
djer13 <djer13@gmail.com>
2020-12-15 07:05:24 +0100

    Use independant Sequence for each entity

--- src/main/java/fr/gamedev/question/data/Answer.java
+++ src/main/java/fr/gamedev/question/data/Answer.java
@@ -5,6 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
 /**
  * @author djer1
  *
@@ -12,7 +15,16 @@ import javax.persistence.OneToOne;
 @Entity
 public class Answer {
 	
-	@GeneratedValue
+	 @GeneratedValue(generator = "seq_gen_answer")
+	 @GenericGenerator(
+			 name = "seq_gen_answer",
+			 strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+			 parameters = {
+				 	@Parameter(name = "sequence_name", value = "seq_answer"),
+				 	@Parameter(name = "initial_value", value = "0"),
+				 	@Parameter(name = "increment_size", value = "1")
+			 }
+	)
 	@Id
 	private long id;
 	
