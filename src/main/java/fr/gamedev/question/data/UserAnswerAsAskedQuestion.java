1c0cdf810d480b65d2e676dcd8b3fcd21c7886d9
djer13 <djer13@gmail.com>
2021-05-04 08:08:16 +0200

    Added link to documentations

--- src/main/java/fr/gamedev/question/data/UserAnswerAsAskedQuestion.java
+++ src/main/java/fr/gamedev/question/data/UserAnswerAsAskedQuestion.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
 
 /**
+ * UserAnswer representation with easy access to data.
+ * More info on projections : https://www.baeldung.com/spring-data-rest-projections-excerpts.
+ * Documentation of projections :
+ * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#projections.interfaces.
  * @author djer13
- *
  */
 @Projection(name = "askedQuestion", types = { UserAnswer.class })
 public interface UserAnswerAsAskedQuestion {
