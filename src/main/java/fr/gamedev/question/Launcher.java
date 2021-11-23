de8b4206968e9314c5a829a89f2a65a7900293e4
djer13 <djer13@gmail.com>
2021-11-23 11:07:17 +0100

    Add PMD, Checkstyle and the POM config

--- src/main/java/fr/gamedev/question/Launcher.java
+++ src/main/java/fr/gamedev/question/Launcher.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package fr.gamedev.question;
 
 import org.springframework.boot.SpringApplication;
@@ -10,14 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author djer1
  *
  */
+//@SuppressWarnings({ "checkstyle:HideUtilityClassConstructor" })
 @SpringBootApplication
 public class Launcher {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(Launcher.class, args);
-	}
+    /**
+     * @param args command line params
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(Launcher.class, args);
+    }
 
 }
